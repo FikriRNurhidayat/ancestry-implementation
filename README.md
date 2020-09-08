d9b4e36be58c59cf1dd27dab406a8268929b617a
Fikri <FikriRNurhidayat@gmail.com>
2020-09-08 17:05:02 +0700

    [Fikk] Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ancestry-implementation
-Basic Implementation of Ancestry Gem on Rails
+# README
+
+This README would normally document whatever steps are necessary to get the
+application up and running.
+
+* Configuration
+
+```bash
+rake db:create
+rake db:migrate
+rails s
+```
---
+++ app/models/category.rb
@@ -0,0 +1,42 @@
+class Category < ApplicationRecord
+  has_ancestry
+
+  before_save :set_code
+  
+  def self.cluster
+    where("ancestry ~ '^[0-9]+$'")
+  end
+
+  def self.master
+    where(:ancestry => nil)
+  end
+
+  def is_cluster?
+    ancestry.present? && /^[0-9]+$/.match?(ancestry)
+  end
+
+  def is_master?
+    ancestry.blank?
+  end
+
+  private
+
+  def sequence_to_string
+    sprintf('%04d', @sequence)
+  end
+
+  def set_master_code
+    @sequence = self.class.where(:ancestry => nil).length + 1
+    self.code = "CAT-#{sequence_to_string}" 
+  end
+
+  def set_cluster_code
+    @sequence = self.class.cluster.length + 1
+    self.code = "CLU-#{sequence_to_string}"
+  end
+
+  def set_code
+    set_master_code if self.is_master?
+    set_cluster_code if self.is_cluster?
+  end
+end
